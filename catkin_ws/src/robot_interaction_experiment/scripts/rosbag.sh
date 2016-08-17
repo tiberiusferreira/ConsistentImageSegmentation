0809a393c91c3b1e7f7a4d1f72dc3a8ef37bbf70
Tiberio Ferreira <tiberiusferreira@gmail.com>
2016-08-17 12:41:05 +0200

    Made the rosbag.sh script no depend on hardcoded paths.

--- catkin_ws/src/robot_interaction_experiment/scripts/rosbag.sh
+++ catkin_ws/src/robot_interaction_experiment/scripts/rosbag.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 bash -c "roscore &";
-cd /mnt/hgfs/PRE/ConsistentImageSegmentation/catkin_ws; rosbag play --loop -u 40 camera_house_2016-08-05-08-39-41.bag
-
+cd ../../.. ; rosbag play --loop -u 40 camera_house_2016-08-05-08-39-41.bag
